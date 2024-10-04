28f2439d10c628020e54ec2949cf285ad748576a
DarthSidM <mauryasiddharth14@gmail.com>
2024-10-04 20:56:13 +0530

    added ros1 rover pilot shit

---
+++ pilot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/siddharth/pilot_ws/src
+# Build directory: /home/siddharth/pilot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rover_pilot")
