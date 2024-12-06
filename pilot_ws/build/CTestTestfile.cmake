04575397526a6a0815ae9d1a15200c33d385bdf4
ArchitKapoor15 <architkapoor15@gmail.com>
2024-12-06 16:38:31 +0530

    Deleted Pilot_ws

--- pilot_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siddharth/pilot_ws/src
-# Build directory: /home/siddharth/pilot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("rover_pilot")
--- pilot_ws/build/rover_pilot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siddharth/pilot_ws/src/rover_pilot
-# Build directory: /home/siddharth/pilot_ws/build/rover_pilot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
